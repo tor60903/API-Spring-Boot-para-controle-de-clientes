98e8ff62a184cc4b6556cbfdc0da4121c0867d4c
tor60903 <tor60903_@hotmail.com>
2023-02-27 21:46:55 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/repositories/IClienteRepository.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.cotiinformatica.entities.Cliente;
+
+@Repository
+public interface IClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
+
+
